a388d4f1f958cd8b11d4718c15163d51850e7717
Cody Walker <cody@cody-walker.com>
2015-08-21 15:47:09 -0500

    First working prototype

---
+++ swaggerClient/swaggerClient.js
@@ -0,0 +1,56 @@
+module.exports = function(RED) {
+    
+    "use strict";
+    var path = require("path");
+    var swaggerjs = require('swagger-client');
+    
+    function SwaggerClientNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg) {
+            var url = config.url || msg.url;
+            var api = config.api || msg.api;
+            var resource = config.resource || msg.resource;
+            var params = config.params || msg.params;
+            
+            if(! (url && api && resource)){
+                node.error('Missing configuration values', msg);
+                return;
+            }
+            
+            console.log('url: ' + url);
+            console.log('api: ' + api);
+            console.log('resource: ' + resource);
+            console.log('params: ' + JSON.stringify(params));
+            
+            
+            var client = new swaggerjs.SwaggerClient({
+                url: url,
+                success: function() {
+                    client[api][resource](params, {responseContentType: 'application/json'}, function(resp){
+                        msg.statusCode = resp.status;
+                        msg.payload = resp.obj;
+                        node.send(msg);
+                        return;
+                    }, function(err){
+                        msg.statusCode = err.status;
+                        msg.payload = err.obj;
+                        node.send(msg);
+                        return;
+                    })
+                },
+                failure: function(err) {
+                    node.error(err, msg);
+                    return;
+                }
+            });
+        });
+    }
+    
+    RED.nodes.registerType("swagger client",SwaggerClientNode);
+    
+    RED.httpAdmin.get('/swagger-client/reqs/*', function(req, res){
+        var filename = path.join(__dirname , '../node_modules/swagger-client/lib', req.params[0]);
+        res.sendfile(filename);
+    });
+}
